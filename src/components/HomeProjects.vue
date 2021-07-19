e5c7d573675776cece410ca34ff0b9a09b32267d
Norman Lüring <norman.luering@tu-dortmund.de>
2021-07-19 22:32:14 +0200

    changed projects

--- src/components/HomeProjects.vue
+++ src/components/HomeProjects.vue
@@ -5,7 +5,7 @@
       </v-layout>
       <v-layout row wrap>
           <v-flex xs12 sm12 md4>
-              <v-card elevation="12" class="d-flex flex-column mx-auto">
+              <v-card elevation="12" class="d-flex flex-column mx-auto" v-if="!reveal">
                   <v-img src="../assets/img/website_code.png" height="500px">
                         <v-container fill-height fluid>
                             <v-layout fill-height>
@@ -21,22 +21,26 @@
                         <h3> This is the first time I've stepped outside of my comfort zone to invest my leisure time in learning how to build a personal website using a framework I like: Vue.js<br/> No raw HTML, No CSS!</h3>
                     </v-card-text>
                     <v-divider></v-divider>
+                    <v-card-text><h1>TEST</h1></v-card-text>
+                    <v-divider></v-divider>
                     <v-card-actions>
                         <v-btn color="teal accent-4" text @click="reveal=true">Read more</v-btn>
                     </v-card-actions>
-                    <v-expand-transition>
-                        <v-card v-if="reveal" class="transition-fast-in-fast-out v-card--reveal overflow-y-auto d-flex flex-column" style="height: 100%;">
-                            <v-card-text class="pb-0">
-                                More will follow once this webpage has successfully been deployed!
-                            </v-card-text>
-                            <v-divider></v-divider>
-                            <v-card-actions class="pt-0">
-                                <v-btn color="teal accent-4" text @click="reveal=false">Close</v-btn>
-                            </v-card-actions>
+                </v-card>
+            </v-flex>
+            <transition>
+                    <v-flex xs12 sm12 md12 v-if="reveal">
+                            <v-card elevation="24" v-click-outside="onClickOutSide" class="d-flex flex-column mx-auto" outlined height="800px" width="100%">
+                                <v-card-title class="text-h5 white--text grey lighten-1">How to write your first Website with Vue</v-card-title>
+                                <v-card-text>
+                                        <h1>Goals:</h1>
+                                </v-card-text>
+                                <v-spacer></v-spacer>
+                                <v-divider></v-divider>
+                                <v-btn bottom color="teal accent-4" text @click="reveal=false">Close</v-btn>
                             </v-card>
-                        </v-expand-transition>
-                    </v-card>
-          </v-flex>
+                    </v-flex>
+            </transition>
       </v-layout>
   </v-container>
 </template>
@@ -46,7 +50,12 @@ export default {
     name: 'HomeProjects',
     data: () => ({
         reveal: false,
-    })
+    }),
+    methods:{
+        onClickOutSide () {
+            this.reveal = false
+        }
+    }
 };
 </script>
 
